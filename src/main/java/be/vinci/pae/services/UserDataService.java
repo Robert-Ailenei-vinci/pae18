fd3499d6d1e8c5973cd36938ef4bd5cf9af89bd0
Rafael Millor Teniente <rafael.millor@student.vinci.be>
2024-02-21 19:48:39 +0100

    add javadoc

--- src/main/java/be/vinci/pae/services/UserDataService.java
+++ src/main/java/be/vinci/pae/services/UserDataService.java
@@ -9,17 +9,58 @@ import java.util.List;
  */
 public interface UserDataService {
 
+  /**
+   * recup tous les user
+   *
+   * @return liste de user
+   */
   List<User> getAll();
 
+  /**
+   * recup un user
+   *
+   * @param id du user a recup
+   * @return le user
+   */
   User getOne(int id);
 
+  /**
+   * recup un user
+   *
+   * @param login du user a recup
+   * @return le user
+   */
   User getOne(String login);
 
+  /**
+   * cree un nv user
+   *
+   * @param item le user a cree
+   * @return le user cree
+   */
   User createOne(User item);
 
+  /**
+   * return le dernier id +1
+   *
+   * @return le dernier id +1
+   */
   int nextItemId();
 
+  /**
+   * login
+   *
+   * @param login    le login
+   * @param password mdp
+   * @return le user , null si existe pas
+   */
   ObjectNode login(String login, String password);
 
+  /**
+   * cree un user
+   *
+   * @param user a cree
+   * @return le user cree
+   */
   ObjectNode register(User user);
 }
