89b8b4ceb7840504ad018f8275fd68fc03f6be4f
Rafael Millor Teniente <rafael.millor@student.vinci.be>
2024-02-27 19:19:59 +0100

    add CORSFilter class

--- src/main/java/be/vinci/pae/api/filters/CORSFilter.java
+++ src/main/java/be/vinci/pae/api/filters/CORSFilter.java
@@ -1,2 +1,25 @@
-package be.vinci.pae.api.filters;public class CorsFilter {
+package be.vinci.pae.api.filters;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+  @Override
+  public void filter(ContainerRequestContext requestContext,
+      ContainerResponseContext responseContext)
+      throws IOException {
+    MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+    headers.add("Access-Control-Allow-Origin",
+        "*"); // Permettre l'accès à partir de n'importe quelle origine
+    headers.add("Access-Control-Allow-Methods",
+        "GET, POST, DELETE, PUT"); // Méthodes HTTP autorisées
+    headers.add("Access-Control-Allow-Headers", "Content-Type"); // En-têtes autorisés
+  }
 }
