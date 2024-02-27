a0383c66034bceb3a6053bc5414ad295f86fc185
Ailenei Robert <robert.ailenei@student.vinci.be>
2024-02-27 16:38:06 +0100

    feture : make test for 100% coverage for UserUCCImpl

---
+++ src/test/java/be/vinci/pae/utils/TestApplicationBinder.java
@@ -0,0 +1,22 @@
+package be.vinci.pae.utils;
+
+
+import be.vinci.pae.business.controller.UserUCC;
+import be.vinci.pae.business.controller.UserUCCImpl;
+import be.vinci.pae.business.domain.DomainFactory;
+import be.vinci.pae.business.domain.DomainFactoryImpl;
+import be.vinci.pae.services.UserDAO;
+import be.vinci.pae.services.UserDAOImpl;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.mockito.Mockito;
+
+public class TestApplicationBinder extends AbstractBinder {
+
+  @Override
+  protected void configure() {
+    bind(UserUCCImpl.class).to(UserUCC.class);
+    bind(Mockito.mock(UserDAOImpl.class)).to(UserDAO.class);
+    bind(DomainFactoryImpl.class).to(DomainFactory.class);
+    // Add more bindings for other interfaces as needed
+  }
+}
