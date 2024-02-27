d8a65eb030133dbd36926830b67f248b3d033ae7
Rafael Millor Teniente <rafael.millor@student.vinci.be>
2024-02-27 13:57:06 +0100

    fix checkstyle

--- src/main/java/be/vinci/pae/api/AuthsResource.java
+++ src/main/java/be/vinci/pae/api/AuthsResource.java
@@ -9,7 +9,12 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
@@ -23,64 +28,64 @@ import jakarta.ws.rs.core.Response;
 @Path("/auths")
 public class AuthsResource {
 
-    private final Algorithm jwtAlgorithm = Algorithm.HMAC256(Config.getProperty("JWTSecret"));
+  private final Algorithm jwtAlgorithm = Algorithm.HMAC256(Config.getProperty("JWTSecret"));
 
-    /**
-     * This service provides methods for user-related operations.
-     */
-    @Inject
-    private UserUCC myUser;
+  /**
+   * This service provides methods for user-related operations.
+   */
+  @Inject
+  private UserUCC myUser;
 
-    /**
-     * Method for handling user authentication.
-     *
-     * @param json JSON object containing the user's login information. It must contain keys "login"
-     *             and "password".
-     * @return A JSON object representing the user's public information after successful auth
-     * @throws WebApplicationException If login information is missing or incorrect, a
-     *                                 WebApplicationException with the appropriate error code is
-     *                                 thrown.
-     */
-    @POST
-    @Path("login")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public UserDTO login(JsonNode json) {
-        // Get and check credentials
-        if (!json.hasNonNull("login") || !json.hasNonNull("password")) {
-            throw new WebApplicationException("login or password required", Response.Status.BAD_REQUEST);
-        }
-        String login = json.get("login").asText();
-        String password = json.get("password").asText();
-
-        // Try to login
-        UserDTO publicUser = myUser.login(login, password);
-        if (publicUser == null) {
-            throw new WebApplicationException("Login or password incorrect",
-                    Response.Status.BAD_REQUEST);
-        }
-        String token;
-        try {
-            token = JWT.create().withIssuer("auth0")
-                    .withClaim("user", publicUser.getId()).sign(this.jwtAlgorithm);
-            System.out.println("Token " + token);
-        } catch (Exception e) {
-            System.out.println("Unable to create token");
-            return null;
-        }
-        return publicUser;
+  /**
+   * Method for handling user authentication.
+   *
+   * @param json JSON object containing the user's login information. It must contain keys "login"
+   *             and "password".
+   * @return A JSON object representing the user's public information after successful auth
+   * @throws WebApplicationException If login information is missing or incorrect, a
+   *                                 WebApplicationException with the appropriate error code is
+   *                                 thrown.
+   */
+  @POST
+  @Path("login")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public UserDTO login(JsonNode json) {
+    // Get and check credentials
+    if (!json.hasNonNull("login") || !json.hasNonNull("password")) {
+      throw new WebApplicationException("login or password required", Response.Status.BAD_REQUEST);
     }
+    String login = json.get("login").asText();
+    String password = json.get("password").asText();
 
-    @GET
-    @Path("user")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Authorize
-    public UserDTO getUser(@Context ContainerRequestContext requestContext){
-        UserDTO user = (UserDTO) requestContext.getProperty("user");
-
-        if (user == null) {
-            throw new WebApplicationException("User not found", Response.Status.UNAUTHORIZED);
-        }
-        return user;
+    // Try to login
+    UserDTO publicUser = myUser.login(login, password);
+    if (publicUser == null) {
+      throw new WebApplicationException("Login or password incorrect",
+          Response.Status.BAD_REQUEST);
     }
+    String token;
+    try {
+      token = JWT.create().withIssuer("auth0")
+          .withClaim("user", publicUser.getId()).sign(this.jwtAlgorithm);
+      System.out.println("Token " + token);
+    } catch (Exception e) {
+      System.out.println("Unable to create token");
+      return null;
+    }
+    return publicUser;
+  }
+
+  @GET
+  @Path("user")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Authorize
+  public UserDTO getUser(@Context ContainerRequestContext requestContext) {
+    UserDTO user = (UserDTO) requestContext.getProperty("user");
+
+    if (user == null) {
+      throw new WebApplicationException("User not found", Response.Status.UNAUTHORIZED);
+    }
+    return user;
+  }
 }
