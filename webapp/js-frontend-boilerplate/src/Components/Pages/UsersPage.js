7c8b4c27e2c9f71e8573c099447119fddd151b56
Lucas Gregoire <lucas.gregoire@student.vinci.be>
2024-03-01 15:44:34 +0100

    get users front v1

---
+++ webapp/js-frontend-boilerplate/src/Components/Pages/UsersPage.js
@@ -0,0 +1,6 @@
+const UsersPage = () => {
+    const main = document.querySelector('main');
+    main.innerHTML = "Liste de tous les users"
+};
+
+export default UsersPage;
--- webapp/js-frontend-boilerplate/src/Components/Router/routes.js
+++ webapp/js-frontend-boilerplate/src/Components/Router/routes.js
@@ -2,12 +2,14 @@ import HomePage from '../Pages/HomePage';
 import Logout from '../Logout/Logout';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
+import UsersPage from '../Pages/UsersPage';
 
 const routes = {
   '/': HomePage,
   '/login': LoginPage,
   '/register': RegisterPage,
   '/logout': Logout,
+  '/users': UsersPage,
 };
 
 export default routes;
