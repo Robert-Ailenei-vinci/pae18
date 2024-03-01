b6da5a5333b4c637dbcce3d754ed108898efc736
Rafael Millor Teniente <rafael.millor@student.vinci.be>
2024-03-01 17:16:17 +0100

    add: addContact path

--- src/main/java/be/vinci/pae/api/ContactRessource.java
+++ src/main/java/be/vinci/pae/api/ContactRessource.java
@@ -1,5 +1,26 @@
 package be.vinci.pae.api;
 
+import be.vinci.pae.api.filters.Authorize;
+import be.vinci.pae.business.domain.ContactDTO;
+import com.fasterxml.jackson.databind.JsonNode;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Singleton
+@Path("/contacts")
 public class ContactRessource {
 
+  @POST
+  @Path("add")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  @Authorize
+  public ContactDTO addContact(JsonNode json) {
+
+    return;
+  }
 }
