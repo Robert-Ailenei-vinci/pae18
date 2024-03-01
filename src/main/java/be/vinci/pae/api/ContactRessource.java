eda0da6960a383fc1df21cdfad90c33307a01e44
Rafael Millor Teniente <rafael.millor@student.vinci.be>
2024-03-01 17:38:21 +0100

    add: meetContact

--- src/main/java/be/vinci/pae/api/ContactRessource.java
+++ src/main/java/be/vinci/pae/api/ContactRessource.java
@@ -6,9 +6,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.inject.Singleton;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Singleton
 @Path("/contacts")
@@ -21,6 +24,19 @@ public class ContactRessource {
   @Authorize
   public ContactDTO addContact(JsonNode json) {
 
-    return;
+    return null;
+  }
+
+  @PUT
+  @Path("met")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public ContactDTO meetContact(JsonNode json) {
+    //Get and check contact id
+    if (!json.hasNonNull("id_contact")) {
+      throw new WebApplicationException("contact id required", Response.Status.BAD_REQUEST);
+    }
+    String contactId = json.get("id_contact").asText();
+
+    return null;
   }
 }
