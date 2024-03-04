f2fcff6634ebfb211af11db312c1f0f4e0d434ce
LHanquet <145200507+LarsHanquet@users.noreply.github.com>
2024-03-04 11:15:07 +0100

    add: UCC for Entreprise, SchoolYear & Contact + Javadoc

--- src/main/java/be/vinci/pae/business/controller/ContactUCCImpl.java
+++ src/main/java/be/vinci/pae/business/controller/ContactUCCImpl.java
@@ -2,14 +2,22 @@ package be.vinci.pae.business.controller;
 
 import be.vinci.pae.business.domain.Contact;
 import be.vinci.pae.business.domain.ContactDTO;
+import be.vinci.pae.business.domain.EntrepriseDTO;
+import be.vinci.pae.business.domain.SchoolYearDTO;
+import be.vinci.pae.business.domain.UserDTO;
 import be.vinci.pae.services.ContactDAO;
 import jakarta.inject.Inject;
 
-public class ContactUCCImpl implements ContactUCC{
-    @Inject
-    private ContactDAO myContactDAO;
-    @Override
-    public ContactDTO newContact(int userID, int entrepriseId, int schoolYearId) {
-        return (Contact) myContactDAO.createOne(userID, entrepriseId, schoolYearId);
-    }
+/**
+ * This class is an implementation of the {@link ContactUCC} interface.
+ */
+public class ContactUCCImpl implements ContactUCC {
+
+  @Inject
+  private ContactDAO myContactDAO;
+
+  @Override
+  public ContactDTO createOne(UserDTO user, EntrepriseDTO entreprise, SchoolYearDTO schoolYear) {
+    return (Contact) myContactDAO.createOne(user, entreprise, schoolYear);
+  }
 }
