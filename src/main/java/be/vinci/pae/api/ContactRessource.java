4e0038a0a92035bb4d82385be82a333e2743298b
Rafael Millor Teniente <rafael.millor@student.vinci.be>
2024-03-07 01:19:54 +0100

    add: meetContact path

--- src/main/java/be/vinci/pae/api/ContactRessource.java
+++ src/main/java/be/vinci/pae/api/ContactRessource.java
@@ -11,8 +11,6 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.WebApplicationException;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -78,13 +76,15 @@ public class ContactRessource {
   @PUT
   @Path("met")
   @Consumes(MediaType.APPLICATION_JSON)
+  @Authorize
   public ContactDTO meetContact(JsonNode json) {
     //Get and check contact id
-    if (!json.hasNonNull("id_contact")) {
+    if (!json.hasNonNull("id_contact") || json.hasNonNull("meetingType")) {
       throw new WebApplicationException("contact id required", Response.Status.BAD_REQUEST);
     }
-    String contactId = json.get("id_contact").asText();
+    int contactId = json.get("id_contact").asInt();
+    String meetingType = json.get("meetingType").asText();
 
-    return null;
+    return myContactUCC.meetContact(contactId, meetingType);
   }
 }
