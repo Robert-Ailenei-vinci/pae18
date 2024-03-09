c3e6ee30e4dba6be64f61eef9d65c79dff3f8fa2
LHanquet <145200507+LarsHanquet@users.noreply.github.com>
2024-03-09 18:18:26 +0100

    add: EntrepriseResource & Javadoc

---
+++ src/main/java/be/vinci/pae/api/EntrepriseResource.java
@@ -0,0 +1,43 @@
+package be.vinci.pae.api;
+
+import be.vinci.pae.api.filters.Authorize;
+import be.vinci.pae.business.controller.EntrepriseUCC;
+import be.vinci.pae.business.domain.EntrepriseDTO;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.util.List;
+
+/**
+ * This class represents a resource for handling enterprise-related operations. It provides methods
+ * to retrieve information about enterprises.
+ */
+@Singleton
+@Path("/entreprise")
+public class EntrepriseResource {
+
+  /**
+   * The business controller for enterprise-related operations.
+   */
+  @Inject
+  EntrepriseUCC myEntreprise;
+
+  /**
+   * Retrieves a list of all enterprises. This method is accessed via HTTP GET request to the path
+   * "/entreprise/getAll". It returns the list of all enterprises in JSON format. Requires
+   * authorization.
+   *
+   * @return A list of {@link EntrepriseDTO} representing all enterprises.
+   */
+  @GET
+  @Path("getAll")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Authorize
+  public List<EntrepriseDTO> getAll() {
+    System.out.println("getAll");
+    return myEntreprise.getAll();
+  }
+}
