63be0025578de967e3de3258f7845b84c93e9e6a
LHanquet <145200507+LarsHanquet@users.noreply.github.com>
2024-03-09 17:59:17 +0100

    add: dropdown menu for entreprise selection

--- webapp/src/Components/Pages/AddContact.js
+++ webapp/src/Components/Pages/AddContact.js
@@ -3,22 +3,34 @@ import { clearPage, renderPageTitle } from '../../utils/render';
 import Navbar from '../Navbar/Navbar';
 import Navigate from '../Router/Navigate';
 
-const AddContactPage = () => {
+const AddContactPage = async () => {
   clearPage();
   renderPageTitle('Créer un contact');
   renderRegisterForm();
+  await renderEntreprises();
 };
 
 function renderRegisterForm() {
   const main = document.querySelector('main');
   const form = document.createElement('form');
   form.className = 'p-5';
-  const entreprise = document.createElement('input');
-  entreprise.type = 'text';
-  entreprise.id = 'entreprise';
-  entreprise.placeholder = 'Sélectionner une entreprise';
-  entreprise.required = true;
-  entreprise.className = 'form-control mb-3';
+  form.id = 'entreprise-form';
+  // Entreprise Dropdown
+  const dropdown = document.createElement('input');
+  dropdown.type = 'checkbox';
+  dropdown.id = 'dropdown';
+  dropdown.name = 'dropdown';
+  dropdown.className = 'dropdown';
+  dropdown.required = true;
+  dropdown.className = 'form-control mb-3';
+  // Entreprise Label
+  const dropdownLabel = document.createElement('label');
+  dropdownLabel.className = 'for-dropdown';
+  dropdownLabel.htmlFor = 'dropdown';
+  dropdownLabel.textContent = 'Dropdown Menu ';
+  const dropdownIcon = document.createElement('i');
+  dropdownIcon.className = 'uil uil-arrow-down';
+
   const submit = document.createElement('input');
   submit.value = 'Confirmer';
   submit.type = 'submit';
@@ -32,7 +44,9 @@ function renderRegisterForm() {
   const formCheckWrapper = document.createElement('div');
   formCheckWrapper.className = 'mb-3 form-check';
 
-  form.appendChild(entreprise);
+  dropdownLabel.appendChild(dropdownIcon);
+  form.appendChild(dropdown);
+  form.appendChild(dropdownLabel);
   form.appendChild(addEntreprise);
   form.appendChild(formCheckWrapper);
   form.appendChild(cancel);
@@ -41,6 +55,68 @@ function renderRegisterForm() {
   cancel.addEventListener('click', onCancel);
   addEntreprise.addEventListener('click', onAddEntreprise);
   main.appendChild(form);
+}
+
+async function renderEntreprises() {
+
+  const form = document.getElementById('entreprise-form');
+  // Create dropdown content section
+  const dropdownSection = document.createElement('div');
+  dropdownSection.className = 'section-dropdown';
+  // Create dropdown link
+  const dropdownLink = document.createElement('a');
+  dropdownLink.href = '#';
+  dropdownLink.textContent = 'Dropdown Link ';
+  const dropdownLinkIcon = document.createElement('i');
+  dropdownLinkIcon.className = 'uil uil-arrow-right';
+  dropdownLink.appendChild(dropdownLinkIcon);
+  dropdownSection.appendChild(dropdownLink);
+  // Create dropdown sub checkbox
+  const dropdownSubCheckbox = document.createElement('input');
+  dropdownSubCheckbox.type = 'checkbox';
+  dropdownSubCheckbox.id = 'dropdown-sub';
+  dropdownSubCheckbox.name = 'dropdown-sub';
+  dropdownSubCheckbox.className = 'dropdown-sub';
+  dropdownSection.appendChild(dropdownSubCheckbox);
+  // Create dropdown sub content section
+  const dropdownSubSection = document.createElement('div');
+  dropdownSubSection.className = 'section-dropdown-sub';
+  const entrepriseList = await allEntreprises();
+  entrepriseList.forEach(entreprise => {
+    const dropdownSubLink = document.createElement('a');
+    dropdownSubLink.href = '#';
+    dropdownSubLink.textContent = entreprise;
+    const dropdownSubLinkIcon1 = document.createElement('i');
+    dropdownSubLink.appendChild(dropdownSubLinkIcon1);
+    dropdownSubSection.appendChild(dropdownSubLink);
+  });
+  // Append dropdown sub content to dropdown section
+  dropdownSection.appendChild(dropdownSubSection);
+
+  // Append dropdown section to form
+  form.appendChild(dropdownSection);
+}
+
+
+async function allEntreprises() {
+    const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const response = await fetch(`http://localhost:3000/entreprise/getAll`, options);
+
+  if (!response.ok) {
+    if (response.status === 401) {
+      // Display a popup message for incorrect username or password
+      alert('No entreprise have been found');
+    } else {
+      // For other errors, handle them accordingly
+      console.error('An error occurred:', response.statusText);
+    }
+  }
+  return response;
 }
 
 async function onSubmit(e) {
