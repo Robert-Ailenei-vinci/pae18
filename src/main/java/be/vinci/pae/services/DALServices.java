64d4f3331ff8759e2a70aa0ed80444df36377f6e
ali-elhannouti-vinci <ali.elhannouti@student.vinci.be>
2024-03-20 23:20:03 +0100

    refactor: javadoc DALServices

--- src/main/java/be/vinci/pae/services/DALServices.java
+++ src/main/java/be/vinci/pae/services/DALServices.java
@@ -1,7 +1,26 @@
+/**
+ * The DALServices interface provides methods for managing database transactions.
+ * These methods are used to start, commit, and rollback a transaction.
+ */
 package be.vinci.pae.services;
 
 public interface DALServices {
-   void startTransaction();
-   void commitTransaction();
-    void rollbackTransaction();
+
+  /**
+   * Starts a new database transaction.
+   * This method should be called before executing any database operation that needs to be part of a transaction.
+   */
+  void startTransaction();
+
+  /**
+   * Commits the current database transaction.
+   * This method should be called after all database operations of a transaction have been successfully executed.
+   */
+  void commitTransaction();
+
+  /**
+   * Rolls back the current database transaction.
+   * This method should be called if an error occurs during the execution of a database operation that is part of a transaction.
+   */
+  void rollbackTransaction();
 }
