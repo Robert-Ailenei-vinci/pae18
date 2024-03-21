8d599b3d23a49de55929f37c38d821c8b1c08709
LHanquet <145200507+LarsHanquet@users.noreply.github.com>
2024-03-21 23:39:25 +0100

    add: checkUniqueUserEnterpriseSchoolYear method for business checking

--- src/main/java/be/vinci/pae/business/domain/Contact.java
+++ src/main/java/be/vinci/pae/business/domain/Contact.java
@@ -1,28 +1,43 @@
 package be.vinci.pae.business.domain;
 
+import java.util.List;
+
 /**
  * This interface extends {@link ContactDTO} and represents a contact.
  */
 public interface Contact extends ContactDTO {
 
   /**
-   * checkStopFollow.
+   * Checks if the contact has been marked as stopped to follow.
    *
-   * @return true or false.
+   * @return {@code true} if the contact is marked as stopped to follow, {@code false} otherwise.
    */
   boolean checkStopFollow();
 
   /**
-   * checkRefused.
+   * Checks if the contact has been marked as refused.
    *
-   * @return true or false.
+   * @return {@code true} if the contact is marked as refused, {@code false} otherwise.
    */
   boolean checkRefused();
 
   /**
-   * checkMeet.
+   * Checks if the contact has been marked as met.
    *
-   * @return true or false.
+   * @return {@code true} if the contact is marked as met, {@code false} otherwise.
    */
   boolean checkMeet();
+
+  /**
+   * Checks the uniqueness of a contact based on the combination of user, enterprise, and school
+   * year.
+   *
+   * @param userContacts  The list of contacts associated with the user.
+   * @param enterpriseDTO The enterprise associated with the contact.
+   * @param schoolYearDTO The school year associated with the contact.
+   * @return {@code true} if the contact is unique based on the provided user, enterprise, and
+   *     school year, {@code false} otherwise.
+   */
+  boolean checkUniqueUserEnterpriseSchoolYear(List<ContactDTO> userContacts,
+      EntrepriseDTO enterpriseDTO, SchoolYearDTO schoolYearDTO);
 }
