b582d2365afadb2c53f6dd378d6c400025b71668
Rafael Millor Teniente <rafael.millor@student.vinci.be>
2024-03-22 12:27:40 +0100

    add: modify state contact

--- webapp/src/Components/Pages/UserDataPage.js
+++ webapp/src/Components/Pages/UserDataPage.js
@@ -1,5 +1,6 @@
 /* eslint-disable spaced-comment */
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import {
   getRememberMe,
   setAuthenticatedUser,
@@ -9,6 +10,11 @@ import {
 import {clearPage, renderPageTitle} from '../../utils/render';
 import Navbar from '../Navbar/Navbar';
 import Navigate from '../Router/Navigate';
+import {
+  meetContact,
+  refuseContact,
+  stopFollowContact
+} from "./utils/ChangeState";
 
 const UserDataPage = () => {
   clearPage();
@@ -122,7 +128,7 @@ async function renderPersonnalInfoPage() {
   const tbody = document.createElement('tbody');
   const trHead = document.createElement('tr');
 
-  ['Entreprise', 'Appelation', 'Adresse', 'Mail', 'N°Telephone', 'Etat',
+  ['Entreprise', 'Appelation', 'Adresse', 'Mail', 'N°Telephone', 'Etat', ' ',
     'Lieu/Type de rencontre', 'Raison de refus'].forEach(text => {
     const th = document.createElement('th');
     th.textContent = text;
@@ -143,35 +149,199 @@ async function renderPersonnalInfoPage() {
           tr.appendChild(td);
         });
 
-    ['state', 'meetingType', 'reasonForRefusal'].forEach(key => {
-      const td = document.createElement('td');
-      td.textContent = contact[key] || '-';
-      tr.appendChild(td);
-    });
+    // État
+    const tdState = document.createElement('td');
+    tdState.textContent = contact.state || '-';
+    tr.appendChild(tdState);
 
-    // Ajouter le bouton pour changer l'état
+    // Bouton pour changer l'état
     const tdButton = document.createElement('td');
     const button = document.createElement('button');
     button.textContent = 'Changer état';
-    button.className = 'btn btn-info';
-    button.addEventListener('click', () => {
+    button.className = 'btn btn-primary';
+    button.setAttribute('type', 'button');
+    button.setAttribute('data-bs-toggle', 'collapse');
+    button.setAttribute('data-bs-target', '#collapseExample_' + contact.id);
+    button.setAttribute('aria-expanded', 'false');
+    button.setAttribute('aria-controls', 'collapseExample_' + contact.id);
+
+    const divForm = document.createElement('div');
+    divForm.className = 'collapse';
+    divForm.id = 'collapseExample_' + contact.id;
+
+    if (contact.state == "initie") {
+      // Création du formulaire
       const form = document.createElement('form');
       const select = document.createElement('select');
-      ['Rencontré', 'Suivi stoppé', 'Refusé'].forEach(optionText => {
+      select.className = 'form-select'; // Ajoutez des classes Bootstrap si nécessaire
+      ['Rencontré', 'Suivi stoppé'].forEach(optionText => {
         const option = document.createElement('option');
         option.value = optionText;
         option.textContent = optionText;
         select.appendChild(option);
       });
       form.appendChild(select);
-      // Insérer le formulaire entre le contact et celui en dessous
-      table.insertBefore(form, table.childNodes[index * 2 + 2]); // index * 2 + 2 car chaque tr et chaque formulaire compte comme un enfant
-    });
-    tdButton.appendChild(button);
-    tr.appendChild(tdButton);
+
+// Création de la zone de texte pour la raison ou le lieu
+      const extraInput = document.createElement('input');
+      extraInput.type = 'text';
+      extraInput.placeholder = 'Lieu';
+      extraInput.style.display = 'block'; // Par défaut, cachez la zone de texte
+
+// Gérer l'affichage de la zone de texte en fonction de la sélection
+      select.addEventListener('change', () => {
+        if (select.value === 'Rencontré') {
+          extraInput.style.display = 'block'; // Afficher la zone de texte si "Rencontré" ou "Refusé" est sélectionné
+        } else {
+          extraInput.style.display = 'none'; // Masquer la zone de texte pour les autres options
+        }
+      });
+
+// Création du bouton de soumission
+      const submitButton = document.createElement('button');
+      submitButton.textContent = 'Soumettre';
+      submitButton.className = 'btn btn-primary';
+      submitButton.type = 'submit'; // Définir le type sur "submit" pour soumettre le formulaire
+
+// Ajout d'un écouteur d'événements pour gérer la soumission du formulaire
+      form.addEventListener('submit', (event) => {
+        event.preventDefault(); // Empêcher le comportement par défaut du formulaire
+
+        const selectedOption = select.value;
+        let additionalInfo = ''; // Informations supplémentaires à envoyer avec la soumission
+
+        // Si "Rencontré" ou "Refusé" est sélectionné, récupérez le contenu de la zone de texte
+        if (selectedOption === 'Rencontré') {
+          additionalInfo = extraInput.value;
+        }
+
+        // Passer les informations supplémentaires à la fonction appropriée
+        switch (selectedOption) {
+          case 'Rencontré':
+            meetContact(contact.id, additionalInfo);
+            window.location.reload();
+            break;
+          case 'Suivi stoppé':
+            stopFollowContact(contact.id);
+            window.location.reload();
+
+            break;
+          default:
+            // Action par défaut ou erreur
+            alert("Sélectionnez un état");
+        }
+
+        // Fermer le collapsible après la soumission
+        const collapse = document.getElementById(
+            'collapseExample_' + contact.id);
+        const bsCollapse = new bootstrap.Collapse(collapse);
+        bsCollapse.hide();
+      });
+
+// Ajout des éléments au formulaire
+      form.appendChild(extraInput);
+      form.appendChild(submitButton);
+
+// Ajout du formulaire au divForm
+      divForm.appendChild(form);
+
+      tdButton.appendChild(button);
+      tdButton.appendChild(divForm);
+      tr.appendChild(tdButton);
+    }
+
+    if (contact.state == "rencontre") {// Création du formulaire
+      const form = document.createElement('form');
+      const select = document.createElement('select');
+      select.className = 'form-select'; // Ajoutez des classes Bootstrap si nécessaire
+      ['Refusé', 'Suivi stoppé'].forEach(optionText => {
+        const option = document.createElement('option');
+        option.value = optionText;
+        option.textContent = optionText;
+        select.appendChild(option);
+      });
+      form.appendChild(select);
+
+// Création de la zone de texte pour la raison ou le lieu
+      const extraInput = document.createElement('input');
+      extraInput.type = 'text';
+      extraInput.placeholder = 'Raison';
+      extraInput.style.display = 'block'; // Par défaut, cachez la zone de texte
+
+// Gérer l'affichage de la zone de texte en fonction de la sélection
+      select.addEventListener('change', () => {
+        if (select.value === 'Refusé') {
+          extraInput.style.display = 'block'; // Afficher la zone de texte si "Rencontré" ou "Refusé" est sélectionné
+        } else {
+          extraInput.style.display = 'none'; // Masquer la zone de texte pour les autres options
+        }
+      });
+
+// Création du bouton de soumission
+      const submitButton = document.createElement('button');
+      submitButton.textContent = 'Soumettre';
+      submitButton.className = 'btn btn-primary';
+      submitButton.type = 'submit'; // Définir le type sur "submit" pour soumettre le formulaire
+
+// Ajout d'un écouteur d'événements pour gérer la soumission du formulaire
+      form.addEventListener('submit', (event) => {
+        event.preventDefault(); // Empêcher le comportement par défaut du formulaire
+
+        const selectedOption = select.value;
+        let additionalInfo = ''; // Informations supplémentaires à envoyer avec la soumission
+
+        // Si "Rencontré" ou "Refusé" est sélectionné, récupérez le contenu de la zone de texte
+        if (selectedOption === 'Refusé') {
+          additionalInfo = extraInput.value;
+        }
+
+        // Passer les informations supplémentaires à la fonction appropriée
+        switch (selectedOption) {
+          case 'Suivi stoppé':
+            stopFollowContact(contact.id);
+            window.location.reload();
+
+            break;
+          case 'Refusé':
+            refuseContact(contact.id, additionalInfo);
+            window.location.reload();
+
+            break;
+          default:
+            // Action par défaut ou erreur
+            alert("Sélectionnez un état");
+        }
+
+        // Fermer le collapsible après la soumission
+        const collapse = document.getElementById(
+            'collapseExample_' + contact.id);
+        const bsCollapse = new bootstrap.Collapse(collapse);
+        bsCollapse.hide();
+      });
+
+// Ajout des éléments au formulaire
+      form.appendChild(extraInput);
+      form.appendChild(submitButton);
+
+// Ajout du formulaire au divForm
+      divForm.appendChild(form);
+
+      tdButton.appendChild(button);
+      tdButton.appendChild(divForm);
+      tr.appendChild(tdButton);
+    }
+
+    // Lieu/Type de rencontre
+    const tdMeeting = document.createElement('td');
+    tdMeeting.textContent = contact.meetingType || '-';
+    tr.appendChild(tdMeeting);
+
+    // Raison de refus
+    const tdReason = document.createElement('td');
+    tdReason.textContent = contact.reasonForRefusal || '-';
+    tr.appendChild(tdReason);
 
     tbody.appendChild(tr);
-
   });
   table.appendChild(tbody);
 
---
+++ webapp/src/Components/Pages/utils/ChangeState.js
@@ -0,0 +1,105 @@
+/* eslint-disable no-undef */
+
+import {getAuthenticatedUser} from "../../../utils/auths";
+
+const user = getAuthenticatedUser();
+
+async function meetContact(idContact, lieu) {
+  console.log("meetcontact");
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${user.token}`,
+    },
+    body: JSON.stringify({
+      "id_contact": idContact,
+      "meetingType": lieu,
+    }),
+  };
+
+  try {
+    const responseContacts = await fetch(
+        `http://localhost:3000/contacts/meet`, options);
+
+    if (!responseContacts.ok) {
+      throw new Error(
+          `Failed to update contacts: ${responseContacts.statusText}`);
+    }
+
+    const contactsData = await responseContacts.json();
+    return contactsData;
+  } catch (error) {
+    throw new Error(
+        `An error occurred while update contacts: ${error.message}`);
+  }
+}
+
+async function stopFollowContact(idContact) {
+  console.log("stop follow");
+
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${user.token}`,
+    },
+    body: JSON.stringify({
+      "id_contact": idContact,
+    }),
+  };
+
+  try {
+    const responseContacts = await fetch(
+        `http://localhost:3000/contacts/stopFollow`, options);
+
+    if (!responseContacts.ok) {
+      throw new Error(
+          `Failed to update contacts: ${responseContacts.statusText}`);
+    }
+
+    const contactsData = await responseContacts.json();
+    return contactsData;
+  } catch (error) {
+    throw new Error(
+        `An error occurred while update contacts: ${error.message}`);
+  }
+}
+
+async function refuseContact(idContact, reason) {
+  console.log("refude");
+
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${user.token}`,
+    },
+    body: JSON.stringify({
+      "id_contact": idContact,
+      "refusalReason": reason
+    }),
+  };
+
+  try {
+    const responseContacts = await fetch(
+        `http://localhost:3000/contacts/refused`, options);
+
+    if (!responseContacts.ok) {
+      throw new Error(
+          `Failed to update contacts: ${responseContacts.statusText}`);
+    }
+
+    const contactsData = await responseContacts.json();
+    return contactsData;
+  } catch (error) {
+    throw new Error(
+        `An error occurred while update contacts: ${error.message}`);
+  }
+}
+
+export {
+  meetContact,
+  stopFollowContact,
+  refuseContact
+}
