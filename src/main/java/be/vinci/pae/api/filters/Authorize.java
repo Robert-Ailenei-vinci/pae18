0f1029e3b1979df25018071f08a9d2643dabd766
LHanquet <145200507+LarsHanquet@users.noreply.github.com>
2024-04-02 22:35:08 +0200

    add: Javadoc for warnings

--- src/main/java/be/vinci/pae/api/filters/Authorize.java
+++ src/main/java/be/vinci/pae/api/filters/Authorize.java
@@ -7,27 +7,41 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * This annotation is used as a name binding for authorization in JAX-RS resources and methods.
+ * This annotation is used as a name binding for authorization in JAX-RS resources and methods. It
+ * serves as a container for role-specific annotations.
  */
 @NameBinding
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Authorize {
 
+  /**
+   * An inner annotation to specify authorization for student role. This annotation can be applied
+   * to JAX-RS resources and methods.
+   */
   @Retention(RetentionPolicy.RUNTIME)
   @Target({ElementType.METHOD, ElementType.TYPE})
   @interface AuthorizeStudent {
 
   }
 
+  /**
+   * An inner annotation to specify authorization for teacher role. This annotation can be applied
+   * to JAX-RS resources and methods.
+   */
   @Retention(RetentionPolicy.RUNTIME)
   @Target({ElementType.METHOD, ElementType.TYPE})
   @interface AuthorizeTeacher {
 
   }
 
+  /**
+   * An inner annotation to specify authorization for supervisor role. This annotation can be
+   * applied to JAX-RS resources and methods.
+   */
   @Retention(RetentionPolicy.RUNTIME)
   @Target({ElementType.METHOD, ElementType.TYPE})
   @interface AuthorizeSupervisor {
 
   }
 }
+
