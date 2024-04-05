cbdfd0bcc5fe9595e4ce4e6721ca5a83d9ed17d2
Lars Hanquet <lars.hanquet@student.vinci.be>
2024-04-05 11:07:10 +0200

    add: method to check the format of given SignatureDate

--- src/main/java/be/vinci/pae/business/controller/StageUCCImpl.java
+++ src/main/java/be/vinci/pae/business/controller/StageUCCImpl.java
@@ -8,6 +8,8 @@ import be.vinci.pae.services.DALServices;
 import be.vinci.pae.services.StageDAO;
 import be.vinci.pae.utils.LoggerUtil;
 import jakarta.inject.Inject;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class is an implementation of the {@link StageUCC} interface.
@@ -18,6 +20,13 @@ public class StageUCCImpl implements StageUCC {
   private StageDAO stageDAO;
   @Inject
   private DALServices dalServices;
+
+  private static boolean checkDateFormat(String dateString) {
+    String pattern = "(0[1-9]|[12]\\d|3[01])/(0[1-9]|1[0-2])/\\d{4}"; // Validating days: 01-31, months: 01-12
+    Pattern regex = Pattern.compile(pattern);
+    Matcher matcher = regex.matcher(dateString);
+    return matcher.matches();
+  }
 
   @Override
   public StageDTO getOneStageByUserId(int userId) {
@@ -42,6 +51,9 @@ public class StageUCCImpl implements StageUCC {
       if (!((User) contact).checkIsStudent()) {
         throw new BizException(
             "This user is not a student.");
+      }
+      if (!checkDateFormat(signatureDate)) {
+        throw new BizException("The date format is not valid");
       }
       StageDTO stage = stageDAO.createOne(contact.getId(), signatureDate, internshipProject,
           supervisorId, contact.getUserId(), contact.getSchoolYearId());
