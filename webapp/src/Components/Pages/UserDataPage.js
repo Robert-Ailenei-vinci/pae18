1f7a1cf24ab1fb906f645cc6d576a3a3c4b342f0
Rafael Millor Teniente <rafael.millor@student.vinci.be>
2024-04-12 11:36:39 +0200

    refactor: bootstrap table UserDataPage.js

--- webapp/src/Components/Pages/UserDataPage.js
+++ webapp/src/Components/Pages/UserDataPage.js
@@ -78,7 +78,6 @@ async function fetchStageData(user) {
 
     return stageData;
   } catch (error) {
-    alert(error.message);
     return undefined;
   }
 }
@@ -131,6 +130,7 @@ async function renderPersonnalInfoPage() {
 
   // Creating table for contacts
   const table = document.createElement('table');
+  table.className = 'table table-bordered table-striped';
   const thead = document.createElement('thead');
   const tbody = document.createElement('tbody');
   const trHead = document.createElement('tr');
@@ -384,7 +384,7 @@ async function renderPersonnalInfoPage() {
   table.appendChild(tbody);
 
   const stageTable = document.createElement('table');
-  stageTable.className = 'table';
+  stageTable.className = 'table table-bordered table-striped';
   const stageThead = document.createElement('thead');
   const stageTbody = document.createElement('tbody');
   const stageTrHead = document.createElement('tr');
