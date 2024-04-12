4a239695b9552c78a7cd9e921d6e3717ecb04f82
Ailenei Robert <robert.ailenei@student.vinci.be>
2024-04-12 11:46:54 +0200

    Added 100 % test coverage

---
+++ src/main/java/be/vinci/pae/api/SchoolYearRessource.java
@@ -0,0 +1,35 @@
+package be.vinci.pae.api;
+
+import be.vinci.pae.api.filters.Authorize;
+import be.vinci.pae.business.controller.SchoolYearUCC;
+import be.vinci.pae.business.domain.SchoolYearDTO;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.util.List;
+
+@Singleton
+@Path("/schoolYears")
+public class SchoolYearRessource {
+
+  @Inject
+  private SchoolYearUCC schoolYearUCC;
+
+  @GET
+  @Path("getAllSchoolYears")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Authorize
+  public List<SchoolYearDTO> getAllSchoolYears() {
+    return schoolYearUCC.getAllSchoolYears();
+  }
+
+  @GET
+  @Path("getDefaultSchoolYear")
+  @Produces(MediaType.APPLICATION_JSON)
+  public SchoolYearDTO getDefaultSchoolYear() {
+    return schoolYearUCC.getCurrentSchoolYear();
+  }
+}
---
+++ webapp/src/Components/Pages/EntrepriseDetailsPage.js
@@ -0,0 +1,105 @@
+import {clearPage, renderPageTitle} from '../../utils/render';
+import {usePathParams} from "../../utils/path-params";
+import {getAuthenticatedUser} from "../../utils/auths";
+import Navbar from "../Navbar/Navbar";
+import Navigate from "../Router/Navigate";
+import {makeStateClean} from "./utils/MakeStateClean";
+
+const EntrepriseDetailsPage = () => {
+  clearPage();
+  renderPageTitle('Détails de l\'entreprise : ' + usePathParams());
+  renderDetailsEntreprise().then(r => console.log(r));
+};
+
+async function fetchEntrepriseContacts(user) {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${user.token}`,
+    },
+  };
+  const response = await fetch(`http://localhost:3000/entreprise/entrepriseDetailsAllContacts/${usePathParams()}`, options);
+  if (!response.ok) {
+    if (response.status === 401) {
+      alert("Username or password is incorrect. Please try again.");
+    } else {
+      alert("An error occurred:"+response.statusText);
+    }
+  }
+  return response.json();
+}
+
+let isRendering = false;
+async function renderDetailsEntreprise() {
+  if (isRendering) {
+    return;
+  }
+  const user = getAuthenticatedUser();
+  isRendering = true;
+  const allContactsData = await fetchEntrepriseContacts(user);
+  isRendering = false;
+  console.log("Contacts data : "+JSON.stringify(allContactsData));
+
+  const main = document.querySelector('main');
+
+  const nbStudentTitle = document.createElement('h4')
+  nbStudentTitle.textContent = 'Nombre d\'étudiants en stage pour l\'année courante : '
+
+  main.appendChild(nbStudentTitle)
+
+  const tableContactsTitle = document.createElement('h5');
+  tableContactsTitle.textContent = 'Contacts de l\'entreprise : ';
+
+  const table = document.createElement('table');
+
+// Create a header row and append it to the table
+  const headerRow = document.createElement('tr');
+  ['Nom de l\'étudiant','Prénom de l\'étudiant','Année académique','Etat', 'Lieu de rencontre', 'Raison du refus'].forEach(text => {
+    const th = document.createElement('th');
+    th.textContent = text;
+    headerRow.appendChild(th);
+  });
+  table.appendChild(headerRow);
+
+// Create a row for each contact and append it to the table
+  allContactsData.forEach(contact => {
+    const row = document.createElement('tr');
+
+    const firstNameStudentCell = document.createElement('td');
+    firstNameStudentCell.textContent = contact.user.firstlastName || '-';
+    row.appendChild(firstNameStudentCell);
+
+    const lastNameStudentCell = document.createElement('td');
+    lastNameStudentCell.textContent = contact.user.lastName || '-';
+    row.appendChild(lastNameStudentCell);
+
+    const schoolYearCell = document.createElement('td');
+    schoolYearCell.textContent = contact.schoolYearDTO.yearFormat || '-';
+    row.appendChild(schoolYearCell);
+
+    const stateCell = document.createElement('td');
+    makeStateClean(contact.state,stateCell)
+    row.appendChild(stateCell);
+
+    const meetingLocationCell = document.createElement('td');
+    meetingLocationCell.textContent = contact.meetingType || '-';
+    row.appendChild(meetingLocationCell);
+
+    const refusalReasonCell = document.createElement('td');
+    refusalReasonCell.textContent = contact.reasonForRefusal || '-';
+    row.appendChild(refusalReasonCell);
+
+    table.appendChild(row);
+  });
+
+  main.appendChild(tableContactsTitle);
+  main.appendChild(table);
+
+  Navbar();
+
+  Navigate(`/detailsEntreprise/${usePathParams()}`);
+
+}
+
+export default EntrepriseDetailsPage;
