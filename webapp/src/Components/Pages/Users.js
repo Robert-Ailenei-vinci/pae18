2fe55bc77e48e9a20964a679f14d7d495d035335
Rafael Millor Teniente <rafael.millor@student.vinci.be>
2024-04-12 12:45:21 +0200

    add: icon

--- webapp/src/Components/Pages/Users.js
+++ webapp/src/Components/Pages/Users.js
@@ -1,4 +1,3 @@
-
 import { getAuthenticatedUser } from '../../utils/auths';
 import { clearPage, renderPageTitle } from '../../utils/render';
 import baseURL from '../../../config';
@@ -8,7 +7,6 @@ const UsersPage = () => {
     renderPageTitle('Tous les utilisateurs');
     renderAllUsers2();
 };
-
 
 async function fetchAllUsers(user) {
     const options = {
@@ -33,21 +31,28 @@ async function fetchAllUsers(user) {
     }
 }
 
-
-
-// Lorsque le fetch fonctionnera
 async function renderAllUsers2() {
     const userData = getAuthenticatedUser();
     try {
         const usersData = await fetchAllUsers(userData);
         const main = document.querySelector('main');
 
+        // Création du bouton de filtrage
+        const filterButton = document.createElement('button');
+        filterButton.className = 'btn btn-primary m-4'
+        filterButton.innerHTML = '<i class="bi bi-filter"></i> Afficher seulement les étudiants';
+
+        filterButton.addEventListener('click', () => {
+            toggleFilterButton(filterButton, usersData);
+        });
+        main.appendChild(filterButton);
+
         const table = document.createElement('table');
-        table.className = 'user-table';
+        table.className = 'table table-bordered table-striped';
 
         const thead = document.createElement('thead');
         const headerRow = document.createElement('tr');
-        ['Prénom', 'Nom', 'Rôle', 'Année scolaire'].forEach(headerText => {
+        ['-','Prénom', 'Nom', 'Rôle', 'Année scolaire'].forEach(headerText => {
             const th = document.createElement('th');
             th.textContent = headerText;
             headerRow.appendChild(th);
@@ -58,7 +63,7 @@ async function renderAllUsers2() {
         const tbody = document.createElement('tbody');
         usersData.forEach(userU => {
             const row = document.createElement('tr');
-            ['firstName', 'lastName', 'role'].forEach(fieldName => {
+            ['-','firstName', 'lastName', 'role'].forEach(fieldName => {
                 const cell = document.createElement('td');
                 cell.textContent = userU[fieldName];
                 row.appendChild(cell);
@@ -74,6 +79,68 @@ async function renderAllUsers2() {
     } catch (error) {
         console.error('Error rendering all users:', error);
     }
+}
+
+function toggleFilterButton(button, usersData) {
+    if (button.dataset.filtered === "true") {
+        button.innerHTML = '<i class="bi bi-filter"></i> Afficher seulement les étudiants';
+        button.dataset.filtered = "false";
+        renderAllUsers(usersData);
+    } else {
+        button.innerHTML = '<i class="bi bi-filter"></i> Afficher tous les utilisateurs';
+        button.dataset.filtered = "true";
+        renderFilteredUsers(usersData);
+    }
+}
+
+function renderFilteredUsers(usersData) {
+    const main = document.querySelector('main');
+    const table = main.querySelector('table');
+    const tbody = table.querySelector('tbody');
+    tbody.innerHTML = ''; // Effacer le contenu actuel du tbody
+    usersData.forEach(userU => {
+        // Filtrer les utilisateurs pour n'afficher que les étudiants
+        if (userU.role === 'etudiant') {
+
+            const row = document.createElement('tr');
+
+            const detailButton = document.createElement('a');
+            detailButton.href = `/detailsEtudiant/${userU.id}`;
+            detailButton.className ='bi bi-arrow-through-heart-fill'
+
+
+            row.appendChild(detailButton);
+
+            [`firstName`, 'lastName', 'role'].forEach(fieldName => {
+                const cell = document.createElement('td');
+                cell.textContent = userU[fieldName];
+                row.appendChild(cell);
+            });
+            const schoolYearCell = document.createElement('td');
+            schoolYearCell.textContent = userU.schoolYear.yearFormat;
+            row.appendChild(schoolYearCell);
+            tbody.appendChild(row);
+        }
+    });
+}
+
+function renderAllUsers(usersData) {
+    const main = document.querySelector('main');
+    const table = main.querySelector('table');
+    const tbody = table.querySelector('tbody');
+    tbody.innerHTML = ''; // Effacer le contenu actuel du tbody
+    usersData.forEach(userU => {
+        const row = document.createElement('tr');
+        ['-','firstName', 'lastName', 'role'].forEach(fieldName => {
+            const cell = document.createElement('td');
+            cell.textContent = userU[fieldName];
+            row.appendChild(cell);
+        });
+        const schoolYearCell = document.createElement('td');
+        schoolYearCell.textContent = userU.schoolYear.yearFormat;
+        row.appendChild(schoolYearCell);
+        tbody.appendChild(row);
+    });
 }
 
 export default UsersPage;
