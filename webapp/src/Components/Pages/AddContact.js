fce4ca7c0c1f58650021ed3d6c10261e65de287b
LHanquet <145200507+LarsHanquet@users.noreply.github.com>
2024-04-24 10:40:53 +0200

    refactor: font color for better readability

--- webapp/src/Components/Pages/AddContact.js
+++ webapp/src/Components/Pages/AddContact.js
@@ -104,7 +104,7 @@ function createAddEntrepriseButtonContainer() {
 
 function createAddEntrepriseButton() {
   const addEntrepriseButton = document.createElement('button');
-  addEntrepriseButton.className = 'btn btn-primary btn-block bg-custom';
+  addEntrepriseButton.className = 'btn btn-secondary btn-block bg-custom';
   addEntrepriseButton.textContent = 'Ajouter une entreprise';
   addEntrepriseButton.addEventListener('click', () => {
     Navigate('/addEnterprise');
@@ -115,7 +115,7 @@ function createAddEntrepriseButton() {
 function createSubmitButton() {
   const submitButton = document.createElement('button');
   submitButton.type = 'submit';
-  submitButton.className = 'btn btn-primary mr-2 bg-custom';
+  submitButton.className = 'btn btn-secondary mr-2 bg-custom';
   submitButton.textContent = 'Submit';
   return submitButton;
 }
